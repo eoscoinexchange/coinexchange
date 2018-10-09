4f1ef8279684b34543a5051b2abcacfe4f000e2b
WaYungGit <377209368@qq.com>
2018-10-09 09:10:23 +0800

    增加撸seven

--- js/main.js
+++ js/main.js
@@ -680,7 +680,7 @@ function checkluroy(name) {
 	})
 }
 
-function luroy() {
+function luchips() {
 	if (loginflag == 0) {
 		Dialog.init("请先点击登录");
 		return;
@@ -707,6 +707,35 @@ function luroy() {
 			});
 		});
 
+	})
+}
+
+function luseven() {
+	if (loginflag == 0) {
+		Dialog.init("请先点击登录");
+		return;
+	}
+
+	scatter.getIdentity({
+		accounts: [network]
+	}).then(function (identity) {
+		var account = identity.accounts[0];
+		var options = {
+			authorization: account.name + '@' + account.authority,
+			broadcast: true,
+			sign: true
+		};
+
+		eos.contract('xxxsevensxxx', options).then(contract => {
+			contract.signup(account.name, "10000.0000 SEVEN", options).then(function (tx) {
+				Dialog.init('Success!');
+				//getaccountinfo(account.name);
+			}).catch(function (e) {
+				console.log(e);
+				e = JSON.parse(e);
+				Dialog.init('Tx failed: ' + e.error.details[0].message);
+			});
+		});
 	})
 }
 
